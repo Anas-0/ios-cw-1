4f4693028a8e8b49aee5e6e8968047f764ffdf54
Anas-0 <anas.almutawa1@gmail.com>
2022-03-02 20:11:55 +0300

    Done

---
+++ Day 1 Work 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Day 1 Work 2
+//
+//  Created by Abolons on 02/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello my name is anas")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day 1 Work 2/Day_1_Work_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Day_1_Work_2App.swift
+//  Day 1 Work 2
+//
+//  Created by Abolons on 02/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Day_1_Work_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
